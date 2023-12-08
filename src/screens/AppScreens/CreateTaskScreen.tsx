4ee6036708495e154bc58d99d978888923e41afd
haruhiroYakumo <sidenkaitei@gmail.com>
2023-12-08 23:40:54 +0200

    Header component for create task screen.

--- src/screens/AppScreens/CreateTaskScreen.tsx
+++ src/screens/AppScreens/CreateTaskScreen.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Text } from "react-native";
 
 import SafeAreaWrapper from "../../components/common/SafeAreaWrapper";
+import FocusAwareStatusBar from "../../components/common/FocusAwareStatusBar";
 
 const CreateTaskScreen: React.FC = () => {
   return <SafeAreaWrapper>
+    <FocusAwareStatusBar barStyle="dark-content" />
+
     <Text>Create Task Screen</Text>
   </SafeAreaWrapper>;
 };
